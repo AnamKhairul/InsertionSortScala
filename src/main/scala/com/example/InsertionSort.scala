ce5394389e2bfb4c1639ad704425c81d1ac284d0
AnamKhairul <anam741@hotmail.com>
2023-02-11 08:26:05 -0500

    Modifies the sorting algorithm

--- src/main/scala/com/example/InsertionSort.scala
+++ src/main/scala/com/example/InsertionSort.scala
@@ -12,23 +12,22 @@ object InsertionSort {
       // Return if no data to process
       if (input.length <= 0) return input
 
-      // Copy data into a local array for processing
+      // Create an output array for the result
       var output: Array[Int] = new Array[Int](input.length)
 
-      for (i <- 0 to input.length - 1) output(i) = input(i)
-
-      // Sort data
-      for (i <- 1 to output.length - 1) {
-         var value: Int = output(i)
-
-         if (order) { // Sort in ascending order
+      if (order) { // Sort in ascending order
+         for (i <- 0 to input.length - 1) {
+            val value: Int = input(i)
             var j = i - 1
             while (j >= 0 && value < output(j)) {
                output(j + 1) = output(j)
                j = j - 1
             }
             output(j + 1) = value
-         } else { // Sort in descending order
+         }
+      } else { // Sort in descending order
+         for(i <- 0 to input.length-1) {
+            val value: Int = input(i)
             var j = i - 1
             while (j >= 0 && value > output(j)) {
                output(j + 1) = output(j)
